0f8f36549216045b9ac75ba8b9c270574ee01c52
Starinin Andrey <starininandrey@gmail.com>
2023-09-10 21:56:23 +0300

    Добавление класса VerificationResult

---
+++ PhotoVerification.Library/VerificationResult.cs
@@ -0,0 +1,14 @@
+﻿namespace PhotoVerification.Library;
+
+public class VerificationResult
+{
+    public bool? ExifNull { get; set; }
+
+    public bool? CameraID { get; set; }
+    public bool? CameraMake { get; set; }
+    public bool? CameraModel { get; set; }
+
+    public bool? Software { get; set; }
+
+    public bool? Data { get; set; }
+}
