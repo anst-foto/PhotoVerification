5934d4679d781dc8f226b8a6815c9dca91831a8c
Starinin Andrey <starininandrey@gmail.com>
2023-09-10 22:42:59 +0300

    Добавление класса VerificationItem

---
+++ PhotoVerification.Library/VerificationItem.cs
@@ -0,0 +1,7 @@
+﻿namespace PhotoVerification.Library;
+
+public class VerificationItem
+{
+    public bool? Exist { get; set; }
+    public string? Value { get; set; }
+}
--- PhotoVerification.Library/VerificationResult.cs
+++ PhotoVerification.Library/VerificationResult.cs
@@ -4,11 +4,10 @@ public class VerificationResult
 {
     public bool? ExifNull { get; set; }
 
-    public bool? CameraID { get; set; }
-    public bool? CameraMake { get; set; }
-    public bool? CameraModel { get; set; }
+    public VerificationItem? CameraID { get; set; }
+    public VerificationItem? CameraMake { get; set; }
+    public VerificationItem? CameraModel { get; set; }
 
-    public bool? Software { get; set; }
-
-    public bool? Data { get; set; }
+    public VerificationItem? ExifSoftware { get; set; }
+    public VerificationItem? XmpPhotoshop { get; set; }
 }
